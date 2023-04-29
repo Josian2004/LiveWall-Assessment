be2c00fe4f39dbaf25133d6845df76f8d55e584b
Josian van Efferen <github@Josian.nl>
2023-04-29 14:47:38 +0200

    Initial Commit

---
+++ LiveWallAssessment/LiveWallAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWallAssessmentApp.swift
+//  LiveWallAssessment
+//
+//  Created by Josian van Efferen on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveWallAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
